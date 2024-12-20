2323f4f742c7a57af6cebb609b319dfe1ccef7fa
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2024-12-20 23:34:38 +0000

    WIP

--- stowables/bash/.bash_profile
+++ stowables/bash/.bash_profile
@@ -5,6 +5,7 @@ shopt -q login_shell && [ -f "${HOME}/.bashrc" ] && source ${HOME}/.bashrc
 # loads hostname specific configurations
 case $(uname -n) in
   gl-dell | gl-red)
-    [[ $(tty) == '/dev/tty1' && -z ${WAYLAND_DISPLAY} && ${XDG_VTNR} -eq 1 ]] && dbus-run-session Hyprland || true
+    # [[ $(tty) == '/dev/tty1' && -z ${WAYLAND_DISPLAY} && ${XDG_VTNR} -eq 1 ]] && dbus-run-session Hyprland || true
+    [[ -t 0 && $(tty) == '/dev/tty1' && ! $DISPLAY ]] && exec startx
   ;;
 esac
--- stowables/neovim/init.lua
+++ stowables/neovim/init.lua
@@ -145,6 +145,29 @@ require("lazy").setup({
 	},
 
 	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+
+	-- {
+	-- 	"nvim-neo-tree/neo-tree.nvim",
+	-- 	branch = "v3.x",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	-- 	},
+	-- },
+
+	{
 		"ggandor/leap.nvim",
 		dependencies = { "tpope/vim-repeat" },
 		config = function()
@@ -1226,6 +1249,7 @@ require("lazy").setup({
 				"query",
 				"vim",
 				"vimdoc",
+				"xml",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
