d7f333f18ee410b995be91606126d925773c163e
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2024-12-23 22:37:51 +0000

    WIP

--- stowables/neovim/init.lua
+++ stowables/neovim/init.lua
@@ -5,6 +5,7 @@ vim.g.loaded_node_provider = 0 -- FIXME: temporary to test performance on macos
 vim.g.loaded_perl_provider = 0 -- FIXME: temporary to test performance on macos
 vim.g.loaded_python_provider = 0 -- FIXME: temporary to test performance on macos
 vim.g.loaded_ruby_provider = 0 -- FIXME: temporary to test performance on macos
+vim.g.loaded_python3_provider = 0 -- FIXME: temporary to test performance on macos
 -- mouse support
 vim.opt.mouse = "a"
 -- temporary files
