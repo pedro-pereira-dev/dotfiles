138d9dc7d9a755bd932e41ae0ee2613aed4d5ab4
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-02-04 09:21:02 +0000

    chore: wip

--- stowables/neovim/init.lua
+++ stowables/neovim/init.lua
@@ -429,80 +429,80 @@ require("lazy").setup({
 		},
 
 		-- status bar
-		{
-			"nvim-lualine/lualine.nvim",
-			dependencies = { "nvim-tree/nvim-web-devicons" },
-			event = { "BufReadPre", "BufNewFile" },
-			opts = {
-				options = {
-					icons_enabled = true,
-					theme = "auto",
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "", right = "" },
-					disabled_filetypes = {
-						statusline = { "NvimTree" },
-						winbar = {},
-					},
-					ignore_focus = {},
-					always_divide_middle = true,
-					always_show_tabline = true,
-					globalstatus = false,
-					refresh = {
-						statusline = 100,
-						tabline = 100,
-						winbar = 100,
-					},
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
-					lualine_x = { "encoding", "fileformat", "filetype" },
-					lualine_y = { "progress" },
-					lualine_z = { "location" },
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = { "filename" },
-					lualine_x = { "location" },
-					lualine_y = {},
-					lualine_z = {},
-				},
-				tabline = {},
-				winbar = {
-					-- lualine_a = { "buffers" },
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = {},
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = {},
-				},
-				inactive_winbar = {},
-				extensions = {},
-			},
-		},
+		-- {
+		-- 	"nvim-lualine/lualine.nvim",
+		-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- 	event = { "BufReadPre", "BufNewFile" },
+		-- 	opts = {
+		-- 		options = {
+		-- 			icons_enabled = true,
+		-- 			theme = "auto",
+		-- 			component_separators = { left = "", right = "" },
+		-- 			section_separators = { left = "", right = "" },
+		-- 			disabled_filetypes = {
+		-- 				statusline = { "NvimTree" },
+		-- 				winbar = {},
+		-- 			},
+		-- 			ignore_focus = {},
+		-- 			always_divide_middle = true,
+		-- 			always_show_tabline = true,
+		-- 			globalstatus = false,
+		-- 			refresh = {
+		-- 				statusline = 100,
+		-- 				tabline = 100,
+		-- 				winbar = 100,
+		-- 			},
+		-- 		},
+		-- 		sections = {
+		-- 			lualine_a = { "mode" },
+		-- 			lualine_b = { "branch", "diff", "diagnostics" },
+		-- 			lualine_c = { "filename" },
+		-- 			lualine_x = { "encoding", "fileformat", "filetype" },
+		-- 			lualine_y = { "progress" },
+		-- 			lualine_z = { "location" },
+		-- 		},
+		-- 		inactive_sections = {
+		-- 			lualine_a = {},
+		-- 			lualine_b = {},
+		-- 			lualine_c = { "filename" },
+		-- 			lualine_x = { "location" },
+		-- 			lualine_y = {},
+		-- 			lualine_z = {},
+		-- 		},
+		-- 		tabline = {},
+		-- 		winbar = {
+		-- 			-- lualine_a = { "buffers" },
+		-- 			lualine_a = {},
+		-- 			lualine_b = {},
+		-- 			lualine_c = {},
+		-- 			lualine_x = {},
+		-- 			lualine_y = {},
+		-- 			lualine_z = {},
+		-- 		},
+		-- 		inactive_winbar = {},
+		-- 		extensions = {},
+		-- 	},
+		-- },
 
 		-- top bar
-		{
-			"akinsho/bufferline.nvim",
-			dependencies = { "nvim-tree/nvim-web-devicons" },
-			-- event = { "BufNewFile", "BufReadPre" },
-			lazy = false,
-			opts = {
-				options = {
-					offsets = {
-						{
-							filetype = "NvimTree",
-							text = function()
-								return vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. "/"
-							end,
-						},
-					},
-				},
-			},
-		},
+		-- {
+		-- 	"akinsho/bufferline.nvim",
+		-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- 	-- event = { "BufNewFile", "BufReadPre" },
+		-- 	lazy = false,
+		-- 	opts = {
+		-- 		options = {
+		-- 			offsets = {
+		-- 				{
+		-- 					filetype = "NvimTree",
+		-- 					text = function()
+		-- 						return vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. "/"
+		-- 					end,
+		-- 				},
+		-- 			},
+		-- 		},
+		-- 	},
+		-- },
 
 		-- other plugins
 	},
