89b9279ec7b3c120d2ab7b75a081ce622f9c24e4
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-09-17 16:56:37 +0100

    moving files around

--- hosts.d/default.gentoo.d/install.sh
+++ hosts.d/default.gentoo.d/install.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 set -eou pipefail
+
+_SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# symlinks dotfiles files to target system
+run_as_root stow "$_SCRIPT_DIR/portage-overlays.conf" /etc/portage/repos.conf/overlays.conf
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# wip
+run_as_root cat <<EOF >/etc/doas.conf
+permit persist  :wheel
+permit nopass   :wheel as root  cmd reboot
+permit nopass   :wheel as root  cmd shutdown
+EOF
+[ -f /etc/doas.conf ] &&
+  run_as_root chmod 0600 /etc/doas.conf &&
+  run_as_root chown root:root /etc/doas.conf &&
+  run_as_root passwd -dl root >/dev/null 2>&1
