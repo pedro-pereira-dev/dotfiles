5ecb54c55583edc6acedc463df1060d52e0769d2
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-09-24 14:28:34 +0100

    removes stuff from macos system

--- hosts.d/bra-m0064.d/bra-m0064.sh
+++ hosts.d/bra-m0064.d/bra-m0064.sh
@@ -6,20 +6,27 @@ function _main() {
   case $_CMD in
 
   configure)
-    (get_option "$_FULL_FLAG" "$@" || get_option "$_INSTALL_FLAG" "$@") &&
+    if get_option "$_FULL_FLAG" "$@" || get_option "$_INSTALL_FLAG" "$@"; then
       run_as_user "$_USER" brew-upgrade
-    # wip
-    # run_as_user "$_USER" secrets-set gpg-github-pedro-pereira-dev
-    # run_as_user "$_USER" secrets-set ssh-gentoo-hetzner-media
-    # run_as_user "$_USER" secrets-set ssh-github-pedro-pereira-dev
-    # run_as_user "$_USER" secrets-set ssh-mercedes-github-pesoare
-    # run_as_user "$_USER" secrets-import
+    fi
+    # if get_option "$_FULL_FLAG" "$@"; then
+    #   run_as_user "$_USER" secrets-set gpg-github-pedro-pereira-dev
+    #   run_as_user "$_USER" secrets-set ssh-authorized-keys
+    #   run_as_user "$_USER" secrets-set ssh-gentoo-hetzner-media
+    #   run_as_user "$_USER" secrets-set ssh-gentoo-laptop
+    #   run_as_user "$_USER" secrets-set ssh-github-pedro-pereira-dev
+    #   run_as_user "$_USER" secrets-set ssh-mercedes-github-pesoare
+    #   run_as_user "$_USER" secrets-import
+    # fi
     return 0
     ;;
 
   setup)
-    run_as_user "$_USER" stow "$_SCRIPT_DIR/layer-homebrew-brewfile.conf" "$_HOME/Brewfile"
     run_as_user "$_USER" stow "$_SCRIPT_DIR/layer-bin-brew-upgrade.sh" "$_HOME/.local/bin/brew-upgrade"
+    run_as_user "$_USER" stow "$_SCRIPT_DIR/layer-homebrew-brewfile.conf" "$_HOME/Brewfile"
+    run_as_user "$_USER" stow "$_SCRIPT_DIR/layer-karabiner.json" "$_HOME/.config/karabiner/karabiner.json"
+    run_as_user "$_USER" stow "$_SCRIPT_DIR/layer-rectangle.json" "$_HOME/.config/rectangle/rectangle.json"
+    run_as_user "$_USER" stow "$_SCRIPT_DIR/layer-ukelele.zip" "$_HOME/.config/ukelele/ukelele.zip"
     return 0
     ;;
 
