eea48ca6cba1db04c2c0e8e1287dea362c2e9241
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-10-25 11:15:22 +0100

    changes gentoo base profile

--- gentoo-base.d/gentoo-base.sh
+++ gentoo-base.d/gentoo-base.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 run_as_root stow "$_HOME/$_DOTS_DIR/gentoo-base.d/gentoo-tools/" '/usr/bin/'
-run_as_root stow "$_HOME/$_DOTS_DIR/gentoo-base.d/system-confs/grub.conf" '/etc/default/grub'
+# run_as_root stow "$_HOME/$_DOTS_DIR/gentoo-base.d/system-confs/grub.conf" '/etc/default/grub'
 run_as_root stow "$_HOME/$_DOTS_DIR/gentoo-base.d/system-confs/overlays.conf" '/etc/portage/repos.conf/overlays.conf'
 run_as_root stow "$_HOME/$_DOTS_DIR/gentoo-base.d/system-confs/package.mask" '/etc/portage/package.mask'
 
