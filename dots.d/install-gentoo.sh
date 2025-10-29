3834be765ea40f9afa812097ae6b03242930a221
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-10-29 10:42:48 +0000

    removes swap

--- dots.d/install-gentoo.sh
+++ dots.d/install-gentoo.sh
@@ -5,7 +5,6 @@ _HOSTNAME=${_HOSTNAME:-'gentoo-system-undefined'}
 _USER=${_USER:-'user'}
 
 _BOOT_SIZE=${_BOOT_SIZE:-'+1G'}
-_SWAP_SIZE=${_SWAP_SIZE:-'+4G'}
 _ROOT_SIZE=${_ROOT_SIZE:-' '} # remaining space
 
 _SMALLEST_DEV_NAME=$(lsblk -bdno NAME,SIZE,TYPE | grep disk | sort -nk2 | head -n1 | cut -d' ' -f1)
@@ -21,14 +20,6 @@ if is_bios; then
        # default first sector
     $_BOOT_SIZE
     a  # bootable flag
-    n  # new partition
-       # default partition type
-       # default partition number
-       # default first sector
-    $_SWAP_SIZE
-    t  # set partition type
-       # default partition number
-    82 # type linux swap
     n  # new partition
        # default partition type
        # default partition number
@@ -50,14 +41,6 @@ else
     n  # new partition
        # default partition number
        # default first sector
-    $_SWAP_SIZE
-    Y  # delete partition signature
-    t  # set partition type
-       # default partition number
-    19 # type linux swap
-    n  # new partition
-       # default partition number
-       # default first sector
     $_ROOT_SIZE
     Y  # delete partition signature
     t  # set partition type
@@ -70,8 +53,7 @@ fi
 
 get_device_partition() { lsblk -f "$_DEV" -nro NAME,TYPE | grep -E "$_SMALLEST_DEV_NAME.*$1 " | cut -d' ' -f1; }
 _BOOT_DEV="/dev/$(get_device_partition 1)"
-_SWAP_DEV="/dev/$(get_device_partition 2)"
-_ROOT_DEV="/dev/$(get_device_partition 3)"
+_ROOT_DEV="/dev/$(get_device_partition 2)"
 
 _TMP_FILE=$(mktemp)
 curl -Lfs -o "$_TMP_FILE" 'https://raw.githubusercontent.com/pedro-pereira-dev/gentoo-installer/refs/heads/main/install.sh'
@@ -79,7 +61,6 @@ sh "$_TMP_FILE" \
   --hostname "$_HOSTNAME" \
   --password "$_PASSWORD" \
   --boot "$_BOOT_DEV" \
-  --swap "$_SWAP_DEV" \
   --root "$_ROOT_DEV" \
   --keymap 'pt-latin9' \
   --timezone 'Europe/Lisbon'
