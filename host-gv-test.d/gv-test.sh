9070d2257ccfce5f233e3a46f08c005df08e3ce6
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-10-31 18:29:37 +0000

    removes stuff from test

--- host-gv-test.d/gv-test.sh
+++ host-gv-test.d/gv-test.sh
@@ -6,9 +6,9 @@ configure() {
   source_file 'shared-base.d/shared-base.sh'
   source_file 'gentoo-base.d/gentoo-base.sh'
 
-  get_option '--full' "$@" && (
-    run_as_root '/usr/bin/eauto' --unsupervised
-    run_as_root eselect news read >/dev/null
-    run_as_root '/usr/bin/regenerate-bootloader'
-  ) || true
+  # get_option '--full' "$@" && (
+  #   run_as_root '/usr/bin/eauto' --unsupervised
+  #   run_as_root eselect news read >/dev/null
+  #   run_as_root '/usr/bin/regenerate-bootloader'
+  # ) || true
 }
