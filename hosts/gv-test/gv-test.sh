90f5f557ca373a5a4079ec3ce88b25fdca0e2378
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-11-19 21:49:29 +0000

    deleter of empty dirs

--- hosts/gv-test/gv-test.sh
+++ hosts/gv-test/gv-test.sh
@@ -9,7 +9,8 @@ configure() {
     run_as_root find / -name '*' -type l 2>/dev/null | while IFS= read -r _LINK; do
       case $_LINK in /dev/* | /proc/* | /run/* | /sys/* | /tmp/*) continue ;; esac
       case $(find "$_LINK" -prune -printf '%l\n' 2>/dev/null)/ in
-      "$_HOME/workspace/personal/dotfiles"/*) rm -v "$_LINK" ;; esac
+      "$_HOME/workspace/personal/dotfiles"/*) rm -v "$_LINK" &&
+      find "$(dirname "$_LINK")" -depth -type d -empty -delete ;; esac
     done
 
   link_as_root "$_HOME/workspace/personal/dotfiles/dots.sh" /usr/bin/dots
