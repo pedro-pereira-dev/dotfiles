ae9d487a82c3f7435ac1d4a0fa7b8a0d2e0739a8
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-11-29 22:26:59 +0000

    WIP

--- files/system-openrc-podman-compose.sh
+++ files/system-openrc-podman-compose.sh
@@ -5,5 +5,5 @@ command="/usr/bin/podman-compose"
 command_args="-f /home/${RC_SVCNAME#*.}/.podman/compose.yaml up -d --force-recreate --remove-orphans"
 
 depend() {
-  need net
+  need dbus elogind net net-online user.${RC_SVCNAME#*.}
 }
