853da3992aa03e2f778d946d44202376ed0028b3
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-11-30 21:05:09 +0000

    podmancompose

--- files/system-openrc-podman-compose.sh
+++ files/system-openrc-podman-compose.sh
@@ -1,22 +1,8 @@
 #!/sbin/openrc-run
 
-_TEST_DOMAIN=one.one.one.one
-_TIMEOUT=30 # seconds
-
 depend() {
   need net-online "user-runtime.${RC_SVCNAME#*.}"
   after sshd
-}
-
-start_pre() {
-  ebegin 'Waiting for DNS'
-  for _ITER in $(seq 1 "${_TIMEOUT}"); do
-    getent hosts ${_TEST_DOMAIN} >/dev/null 2>&1 &&
-      eend 0 'DNS resolution working' && return 0
-    sleep 1
-  done
-  eend 1 'DNS resolution not working'
-  return 1
 }
 
 start() {
