daf25465da12c7a2b3f076f1b1414e738e95f53a
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2025-12-08 21:38:44 +0000

    removing old files

--- old/shared-base.d/shared-base.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-run_as_user "$_USER" stow "$_HOME/$_DOTS_DIR/dots" "$_HOME/.local/bin/dots"
-run_as_user "$_USER" stow "$_HOME/$_DOTS_DIR/shared-base.d/shared-confs/ssh.conf" "$_HOME/.ssh/config"
-run_as_user "$_USER" stow "$_HOME/$_DOTS_DIR/shared-base.d/shared-confs/tmux.conf" "$_HOME/.config/tmux/tmux.conf"
-run_as_user "$_USER" stow "$_HOME/$_DOTS_DIR/shared-base.d/shared-tools/" "$_HOME/.local/bin/"
-run_as_user "$_USER" stow "$_HOME/$_DOTS_DIR/shared-base.d/user-confs/" "$_HOME/"
--- old/shared-desktop.d/shared-desktop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-run_as_user "$_USER" stow "$_HOME/$_DOTS_DIR/shared-desktop.d/desktop-confs/alacritty.toml" "$_HOME/.config/alacritty/alacritty.toml"
