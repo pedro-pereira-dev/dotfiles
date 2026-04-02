1084af1f29c688f74e52d83fdf687c65d9be6397
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2026-04-02 14:54:20 +0100

    nebula sync installer

---
+++ setup-nebula-sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p /tmp/nebula-sync
+curl -Lfs "$(
+  curl -s https://api.github.com/repos/lovelaze/nebula-sync/releases/latest |
+    grep 'browser_download_url.*linux_amd64.tar.gz' | cut -d '"' -f 4
+)" -o /tmp/nebula-sync/nebula-sync.tar.gz
+tar -xf /tmp/nebula-sync/nebula-sync.tar.gz -C /tmp/nebula-sync
+mv /tmp/nebula-sync/nebula-sync /usr/bin/
+chmod +x /usr/bin/nebula-sync
+rm -fr /tmp/nebula-sync
