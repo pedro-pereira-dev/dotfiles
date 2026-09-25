2c1456f63019baee94ab21be06e0bfa5cbc25fec
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2026-09-25 11:21:45 +0100

    update script

--- hosts/bra-m0234.sh
+++ hosts/bra-m0234.sh
@@ -9,14 +9,18 @@ dots_pull() {
 
 dots_sync() {
   link_file "$DOTFILES_WORKSPACE/dots" "$HOME/.local/bin/dots"
+
   link_file "$DOTFILES_WORKSPACE/hosts/bra-m0234.d/brewfile" "$HOME/.Brewfile"
-  link_file "$DOTFILES_WORKSPACE/bin/maintain-homebrew" "$HOME/.local/bin/maintain-homebrew"
-  link_file "$DOTFILES_WORKSPACE/bin/maintain-macos" "$HOME/.local/bin/maintain-macos"
   link_file "$DOTFILES_WORKSPACE/shared/bash_profile.sh" "$HOME/.bash_profile"
   link_file "$DOTFILES_WORKSPACE/shared/bashrc.sh" "$HOME/.bashrc"
+
+  link_file "$DOTFILES_WORKSPACE/bin/maintain-homebrew" "$HOME/.local/bin/maintain-homebrew"
+  link_file "$DOTFILES_WORKSPACE/bin/maintain-macos" "$HOME/.local/bin/maintain-macos"
+  link_file "$DOTFILES_WORKSPACE/bin/update" "$HOME/.local/bin/update"
+
   prune_stale_links
-  "$HOME/.local/bin/maintain-macos" --force
-  "$HOME/.local/bin/maintain-homebrew" --force
+  "$HOME/.local/bin/update" --force
+
   install_darwin_bash
   set_darwin_bash_shell
 }
